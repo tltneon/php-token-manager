a2b0c33f3ab139fb86690d0597744087b78bf74d
gbrousse <pro@gregory-brousse.fr>
2015-11-02 10:42:53 +0100

    Tests update

--- tests/TokenManager/tokenManagerTest.php
+++ tests/TokenManager/tokenManagerTest.php
@@ -7,13 +7,14 @@ class tokenManagerTest extends PHPUnit_Framework_TestCase{
     
     public function __construct($name = null, array $data = array(), $dataName = '') {
         parent::__construct($name, $data, $dataName);
-
-        mkdir($this->tokenDirPath);
+        if(!is_dir($this->tokenDirPath))mkdir($this->tokenDirPath);
     }
     
     public function __destruct() {
-        $this->cleanTokenDir($this->tokenDirPath);
-        rmdir($this->tokenDirPath);
+        if(is_dir($this->tokenDirPath)){
+            $this->cleanTokenDir($this->tokenDirPath);
+            rmdir($this->tokenDirPath);
+        }     
     }
     
     /**
